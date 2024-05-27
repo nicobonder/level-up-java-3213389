1b8ef57f5a6aa2e94ef21d4bfc2fdbe17d06f62d
Nico Bonder <nicobonder@hotmail.com>
2024-05-27 10:14:35 +0000

    person created

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -4,6 +4,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        
+        Person p1 = new Person("Juancho", "Tito", 30);
+        Person p2 = new Person("Negro", "Cabezon", 32);
+
+        System.out.println(p1.greet());
+        System.out.println(p2.greet());
     }
 }
