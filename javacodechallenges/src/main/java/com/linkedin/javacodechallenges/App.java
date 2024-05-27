184c655cc264b48a6a7ce9de60a5267660d21065
Nico Bonder <nicobonder@hotmail.com>
2024-05-27 09:28:39 +0000

    water bill done

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -5,8 +5,22 @@ import java.util.Scanner;
 public class App {
 
     public static double calculateWaterBill(double gallonsUsage) {
+           
         // TODO: Implement method
-        return 0;
+        final double minimunCharge = 18.84;
+        final double extraVolumeCost = 3.90;
+        final double CCFToCharge = CCFUsed(gallonsUsage) - 2;
+        final double extraCharges = CCFToCharge * extraVolumeCost; 
+        return minimunCharge + extraCharges;
+    }
+
+    final static double CCFUsed(double gallonsUsage) {
+        final int CCF = 748;
+        if (gallonsUsage <= 1496) {
+            return 2;
+        } else {
+           return Math.ceil(gallonsUsage / CCF);
+        }
     }
 
     public static void main(String[] args) {
